d456a6e419e37875e25feeaef559c439799f9c7e
Abraham Jimenez <j8wehlbn@duck.com>
2025-01-28 17:59:23 -0700

    Fix analytics at build

--- astro.config.ts
+++ astro.config.ts
@@ -10,6 +10,7 @@ import partytown from '@astrojs/partytown';
 import icon from 'astro-icon';
 import compress from 'astro-compress';
 import type { AstroIntegration } from 'astro';
+import react from '@astrojs/react';
 
 import astrowind from './vendor/integration';
 
@@ -31,6 +32,7 @@ export default defineConfig({
   }),
 
   integrations: [
+    react(),
     tailwind({
       applyBaseStyles: false,
     }),
